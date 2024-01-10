f0522e089da486ce88327c29b6ae435c38da4032
Az3r1y <137179080+Az3r1y@users.noreply.github.com>
2024-01-10 14:23:34 +0100

    Update MainWindow.xaml.cs

--- TestMqttBroker/MainWindow.xaml.cs
+++ TestMqttBroker/MainWindow.xaml.cs
@@ -1,45 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MQTTnet;
 using MQTTnet.Client;
-using MQTTnet.Packets;
-using MQTTnet.Protocol;
 using TestMqttBroker.Services;
 
 namespace TestMqttBroker
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private dao_mqtt dao_Mqtt;
-       
-
+        private ObservableCollection<string> mqttMessages = new ObservableCollection<string>();
 
         public MainWindow()
         {
             InitializeComponent();
             dao_Mqtt = new dao_mqtt();
-
-
             Task.Run(async () => await dao_Mqtt.Connect()).Wait();
-            dao_Mqtt.Subscribe("topic/test");
+            dao_Mqtt.Subscribe("smartdisplay/sound");
+            dao_Mqtt.MessageReceived += OnMqttMessageReceived;
+            mqttDataGrid.ItemsSource = mqttMessages;
+        }
 
+        private void OnMqttMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
+        {
+            string message = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+            Dispatcher.Invoke(() =>
+            {
+                mqttMessages.Add(message);
+            });
         }
     }
 }
-
-
